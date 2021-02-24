fd90e34e0081d2dbb2c0915c6fe2d8a3e5dd2bf3
Viktoria Azarenko <starlet.viola@gmail.com>
2021-02-24 21:01:27 +0200

    routing

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,21 @@ Route::get('/', function () {
 
 Route::get('/hello', function () {
     return 'hello world!';
+})->name('hello');
+
+Route::get('/home', function () {
+    return view('home');
+})->name('homep');
+
+Route::prefix('admin')->name('admin.')->group( function (){
+    Route::get('/posts/{id}/edit', function ($id) {
+        return 'Post #'.$id;
+    });
+    Route::get('/home', function () {
+        return 'home test page';
+    })->name('homep');
+});
+
+Route::fallback(function () {
+   return redirect()->route('homep');
 });
