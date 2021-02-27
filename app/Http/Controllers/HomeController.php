43aff98b4b3f470ac63ca12b9277d111c78de7b8
Viktoria Azarenko <starlet.viola@gmail.com>
2021-02-27 23:17:11 +0200

    problem with migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 class HomeController extends Controller {
 
     public function index(){
+//        dump($_ENV);
+        dump(config('database'));
         return "Hello world";
     }
 
